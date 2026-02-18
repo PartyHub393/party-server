95b8ce036e1d01219d2afc0597328d4894762ac8
Pike <pikentk@gmail.com>
2026-02-17 23:19:43 -0500

    Update Database Connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,16 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  //host: process.env.PGHOST || 'aws-1-us-east-2.pooler.supabase.com',
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT || 5432,
-  //database: process.env.PGDATABASE || 'postgres',
-  database: process.env.PGDATABASE || 'postgres-discovercase',
-  //user: process.env.PGUSER || 'postgres.fvzxeyflnzpyqcdayacc',
-  user: process.env.PGUSER || 'postgres',
-  //password: process.env.PGPASSWORD || 'P9lVFj2ZM1AscMFE',
-  password: process.env.PGPASSWORD || 'postgres',
-  pool: process.env.POOLMODE || 'session'
+  host: process.env.PGHOST || 'aws-1-us-east-2.pooler.supabase.com',
+  database: process.env.PGDATABASE || 'postgres',
+  user: process.env.PGUSER || 'postgres.fvzxeyflnzpyqcdayacc',
+  password: process.env.PGPASSWORD || 'P9lVFj2ZM1AscMFE',
+  port: 5432
 });
 
 const createUserTable = async () => {
@@ -25,7 +20,7 @@ const createUserTable = async () => {
   `;
   try {
     await pool.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-    await pool.query(queryText);
+    await pool.query(queryText); 
     console.log("User table is ready.");
   } catch (err) {
     console.error("Error creating user table:", err.message || err);
