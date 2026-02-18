9470ac26b28a27f165080d93bda175b30561a22e
aashleyschen <aashleyschen@gmail.com>
2026-02-18 14:34:07 -0500

    fixed spacing for trivia card

--- frontend/src/components/games/Trivia.jsx
+++ frontend/src/components/games/Trivia.jsx
@@ -9,7 +9,7 @@ export default function Trivia() {
             <div className="category-selection">
                 <h2>Select a Category</h2>
                 <div className="category-cards">
-                    <div className="category-card">
+                    <div className="category-card full-width">
                         <h3>Case Trivia</h3>
                         <p>Test your knowledge of Case and it's history!</p>
                     </div>
